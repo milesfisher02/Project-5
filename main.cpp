99a167c9d50ac5764335b7866b84747d565edae7
milesfisher02 <145312573+milesfisher02@users.noreply.github.com>
2023-12-04 01:54:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/* Programming Assignment #5
+Name: Miles Fisher
+Class: CS 250
+School: St. Thomas Aquinas College
+Program Description: A program that inputs a file with the amount of dogs within the country and the world and
+displays a menu with choices of what the user wants to see... total nnumber of dogs in the world,
+or dogs in a given country */
 #include "Dogs.h"
 #include <iostream>
 
